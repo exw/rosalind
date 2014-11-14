f23940c0042d08a0c59ff1d004a9f1013a0f41db
exw <exw@users.noreply.github.com>
2014-11-13 19:06:21 -0600

    Update 11GRPH.py

--- src/11GRPH.py
+++ src/11GRPH.py
@@ -34,13 +34,20 @@ for source in data:
 	for target in data:
 		if data[targetIndex] == data[sourceIndex]:
 			pass
-		elif source[-1:-4:-1] == target[0:3]:
-			answer.append(titles[sourceIndex] + ' ' + titles[targetIndex])
-			test.append("next pair: " + data[sourceIndex] + '\n' + data[targetIndex])
+		elif source.endswith(target[0:3]):
+			answer.append([titles[sourceIndex],titles[targetIndex]])
 		targetIndex = targetIndex + 1
 	sourceIndex = sourceIndex + 1
 
-
+# Check for duplicates
+for pair in answer:
+	for checked in answer:
+		if pair[0] == checked[1] and pair[1] == checked[0]:
+			answer.remove(pair)
+			
+formatted = []
 # Format answer
 for found in answer:
-	print found
+	formatted.append(found[0] + ' ' + found[1])
+for ans in formatted:
+	print ans
